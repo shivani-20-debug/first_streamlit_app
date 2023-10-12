30b9895a14293b7555e90ade1ab6fe0415bd8313
shivani-20-debug <68936063+shivani-20-debug@users.noreply.github.com>
2023-10-13 10:58:20 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+add_fruit = streamlit.text_input('What fruit would you like information about?', 'Jackfruit')
+streamlit.write('Thanks for adding jackfruit')
+
 
