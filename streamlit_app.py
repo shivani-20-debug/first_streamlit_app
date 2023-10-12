f9794161bff799a2a571cb6de9c23e6ea962f0cd
shivani-20-debug <68936063+shivani-20-debug@users.noreply.github.com>
2023-10-12 18:05:59 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,11 +38,11 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]
-                                     my_cur = my_cnx.cursor()
+my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.datafram(my_data_rows)
+streamlit.dataframe(my_data_rows)
 
 
 
