e561eb24dd281201b586226f3905fd2f47d8ff3d
shivani-20-debug <68936063+shivani-20-debug@users.noreply.github.com>
2023-10-12 14:22:27 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,7 @@ streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
-streamlit.text(fruityvice_response.json())
+
 
 
 
