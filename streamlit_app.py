e0ee12b13484678bc9242124c1063d117affd48f
shivani-20-debug <68936063+shivani-20-debug@users.noreply.github.com>
2023-10-12 14:27:44 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,13 +22,18 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
-streamlit.text(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
