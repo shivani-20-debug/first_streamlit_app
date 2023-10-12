4a3f4b0d0fb91c671c49345edbd018e0540bf237
shivani-20-debug <68936063+shivani-20-debug@users.noreply.github.com>
2023-10-12 14:24:20 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ streamlit.text(fruityvice_response.json()
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
 
 
 
