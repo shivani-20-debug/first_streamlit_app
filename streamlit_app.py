3ca79ab18cdfe8d6c65a1c258039fc4eb866cb23
shivani-20-debug <68936063+shivani-20-debug@users.noreply.github.com>
2023-10-13 11:28:44 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.text(fruityvice_response)
 def get_fruityvice_data(this_fruit_choice):
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-return fruityvice_normalized  
+      return fruityvice_normalized  
 
 
 streamlit.header('Fruityvice Fruit Advice!')
