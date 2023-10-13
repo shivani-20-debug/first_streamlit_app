8999788259520c18388067a622f70cce1b7caf8c
shivani-20-debug <68936063+shivani-20-debug@users.noreply.github.com>
2023-10-13 12:12:09 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ except URLError as e:
 #import snowflake.connector
 
 def get_fruit_load_list():
-      with my_cnx.cursor as my_cur:
+      with my_cnx.cursor() as my_cur:
             my_cur.execute("SELECT * from fruit_load_list")
             return my_cur.fetchall()
 
